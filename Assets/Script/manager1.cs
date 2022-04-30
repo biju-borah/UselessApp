718aaffeedb03767a4cca7d83f653410d8c0787b
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-04-30 23:54:51 +0530

    initial commit

---
+++ Assets/Script/manager1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class manager1 : MonoBehaviour
+{
+    public GameObject screen1;
+    public manager2 screen2;
+    // Start is called before the first frame update
+    void Start()
+    {
+       screen1.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void go()
+    {
+        screen1.SetActive(false);
+        screen2.enablescreen();
+
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Script/manager2.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class manager2 : MonoBehaviour
+{
+    public GameObject screen;
+    public string ch1;
+    public string ch2;
+    public string ma;
+    public string yo;
+    public string cs;
+    public string me;
+    public string ev;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void enablescreen()
+    {
+        screen.SetActive(true);
+    }
+    public void open1()
+    {
+        Application.OpenURL(ch1);
+    }
+    public void open2()
+    {
+        Application.OpenURL(ma);
+    }
+    public void open3()
+    {
+        Application.OpenURL(yo);
+    }
+    public void open4()
+    {
+        Application.OpenURL(ch2);
+    }
+    public void open5()
+    {
+        Application.OpenURL(me);
+    }
+    public void open6()
+    {
+        Application.OpenURL(ev);
+    }
+    public void open7()
+    {
+        Application.OpenURL(cs);
+    }
+}
